909c14d5bb48acf2d5da4aa57570cd2176d4968e
Angeline Chooi <65768910+acwyo@users.noreply.github.com>
2025-08-26 14:06:25 +0800

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE login(
+username nvarchar(50),
+password nvarchar(50)
+)
+
+INSERT INTO login (username, password)
+VALUES ('user1', 'pwd1')
+
+INSERT INTO login (username, password)
+VALUES ('user2', 'pwd2')
